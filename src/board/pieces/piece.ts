9d3f9f90bffa69a19106b0658f0ed44bd3c19d49
Biszku <dbichu@onet.pl>
2023-10-10 21:18:57 +0200

    added move logic to pawn

--- src/board/pieces/piece.ts
+++ src/board/pieces/piece.ts
@@ -1,22 +1,50 @@
 class Piece {
-  // startingCol;
-  // startingRow;
   curCol;
   curRow;
   name;
   color;
+  moved?;
 
   constructor(col: number, row: number, name: string) {
-    // this.startingCol = col;
-    // this.startingRow = row;
     this.curCol = col;
     this.curRow = row;
     this.name = name;
-    this.color = col <= 4 ? "black" : "white";
+    this.color = row > 4 ? "black" : "white";
+    if (name.includes("pawn") || name.includes("king")) this.moved = false;
   }
 
   render() {
     return `<img src=${this.color}-${this.name}.png  />`;
+  }
+
+  changeCords(cords: number[]) {
+    this.curCol = cords[0];
+    this.curRow = cords[1];
+
+    if (this.name.includes("pawn") || this.name.includes("king"))
+      this.moved = true;
+  }
+
+  legalMoves() {
+    const legalMoves = [];
+
+    switch (this.name) {
+      case "pawn":
+        legalMoves.push([
+          this.curCol,
+          this.color === "white" ? this.curRow + 1 : this.curRow - 1,
+        ]);
+
+        if (!this.moved)
+          legalMoves.push([
+            this.curCol,
+            this.color === "white" ? this.curRow + 2 : this.curRow - 2,
+          ]);
+
+        return legalMoves;
+      default:
+        console.log("Piece doesn't exist!");
+    }
   }
 }
 
