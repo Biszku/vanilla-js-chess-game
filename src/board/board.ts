149d258014950be810bd05c26fa1ba316d45479b
Biszku <dbichu@onet.pl>
2023-10-10 21:18:43 +0200

    fixed bugs and cleaned little code

--- src/board/board.ts
+++ src/board/board.ts
@@ -27,8 +27,9 @@ class Board {
     curCol: number;
     curRow: number;
     name: string;
-    // startingCol: number;
-    // startingRow: number;
+    render: () => string;
+    legalMoves: () => number[][];
+    changeCords: (cords: number[]) => void;
   }[] = [];
 
   creatingBoard() {
@@ -37,29 +38,43 @@ class Board {
     this.createFieldsAssignment();
   }
 
-  private createFieldsAssignment() {
-    const Fields = document.querySelectorAll(".chess-field");
-    const dividedFields = [];
+  private renderMainBoard() {
+    const MainBoard = [];
+    let curInitColor = this.initialColor;
 
-    for (let i = 0; i <= 63; i += 8) {
-      dividedFields.push([...Fields].slice(i, i + 8));
-    }
+    for (let rows = this.rows; rows >= 1; rows--) {
+      let curColor = curInitColor;
 
-    const rightArr = dividedFields.reverse().flat(2);
-
-    for (let cols = 1; cols <= this.columns; cols++) {
-      for (let rows = 1; rows <= this.rows; rows++) {
-        const index = (cols - 1) * 8 + (rows - 1);
-        this.boardState.set(rightArr[index], [cols, rows]);
+      for (let cols = 1; cols <= this.columns; cols++) {
+        const renderedField = this.renderField(curColor, cols, rows);
+        MainBoard.push(renderedField);
+        curColor = curColor === "white" ? "black" : "white";
       }
+      curInitColor = curInitColor === "white" ? "black" : "white";
     }
+    this.renderElement(MainBoard.join(""));
   }
 
-  private renderElement(comps: string, location: string | null = null) {
-    this.parentElement.insertAdjacentHTML(
-      location === "outside" ? "beforebegin" : "afterbegin",
-      comps
-    );
+  private renderField(color: string, col: number, row: number) {
+    const colorClass = color === "black" ? "black-field" : "white-field";
+
+    return `<div class="chess-field ${colorClass}">${this.renderPiece(
+      col,
+      row
+    )}</div>`;
+  }
+
+  private renderPiece(col: number, row: number) {
+    const piece =
+      row === 1 || row === 8
+        ? new Piece(col, row, this.piecePattern[col - 1])
+        : row === 2 || row === 7
+        ? new Piece(col, row, "pawn")
+        : null;
+
+    if (!piece) return "";
+    this.pieceState.push(piece);
+    return piece.render();
   }
 
   private renderBounds() {
@@ -83,40 +98,29 @@ class Board {
       .join("")}</div>`;
   }
 
-  private renderMainBoard() {
-    const MainBoard = [];
-    let curInitColor = this.initialColor;
+  private renderElement(comps: string, location: string | null = null) {
+    this.parentElement.insertAdjacentHTML(
+      location === "outside" ? "beforebegin" : "afterbegin",
+      comps
+    );
+  }
 
-    for (let cols = 1; cols <= this.columns; cols++) {
-      let curColor = curInitColor;
-      for (let rows = 1; rows <= this.rows; rows++) {
-        const renderedField = this.renderField(curColor, cols, rows);
+  private createFieldsAssignment() {
+    const Fields = document.querySelectorAll(".chess-field");
+    const dividedFields = [];
 
-        MainBoard.push(renderedField);
-        curColor = curColor === "white" ? "black" : "white";
-      }
-      curInitColor = curInitColor === "white" ? "black" : "white";
+    for (let i = 0; i <= 63; i += 8) {
+      dividedFields.push([...Fields].slice(i, i + 8));
     }
-    this.renderElement(MainBoard.join(""));
-  }
 
-  private renderField(color: string, col: number, row: number) {
-    const colorClass = color === "black" ? "black-field" : "white-field";
-    return `<div class="chess-field ${colorClass}">${this.renderPiece(
-      col,
-      row
-    )}</div>`;
-  }
+    const rightArr = dividedFields.reverse().flat(2);
 
-  private renderPiece(col: number, row: number) {
-    const piece =
-      col === 1 || col === 8
-        ? new Piece(col, row, this.piecePattern[row - 1])
-        : col === 2 || col === 7
-        ? new Piece(col, row, "pawn")
-        : "";
-    if (piece) this.pieceState.push({ ...piece });
-    return piece && piece.render();
+    for (let rows = 1; rows <= this.rows; rows++) {
+      for (let cols = 1; cols <= this.columns; cols++) {
+        const index = (rows - 1) * 8 + (cols - 1);
+        this.boardState.set(rightArr[index], [cols, rows]);
+      }
+    }
   }
 }
 
