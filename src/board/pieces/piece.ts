37086dff5b32e075fb7894b145f3a38c88a1b342
Biszku <dbichu@onet.pl>
2023-11-04 16:52:23 +0100

    fixed cross moves bug,code optimization

--- src/board/pieces/piece.ts
+++ src/board/pieces/piece.ts
@@ -1,20 +1,19 @@
-import { PieceStateArr } from "../board";
+import { PieceStateArr } from "../../lib/definitions";
 
 class Piece {
-  curCol;
-  curRow;
-  name;
+  col;
+  row;
   color;
+  name;
+  moved?;
   possibleMoves: [number, number, (string | undefined)?][] = [];
   attackedFields: number[][] | null = null;
-  moved?;
 
   constructor(col: number, row: number, name: string) {
-    this.curCol = col;
-    this.curRow = row;
+    this.col = col;
+    this.row = row;
     this.name = name;
     this.color = row > 4 ? "black" : "white";
-    this.setAttackFields(name);
     if (name.includes("pawn") || name.includes("king")) this.moved = false;
   }
 
@@ -22,14 +21,8 @@ class Piece {
     switch (type) {
       case "pawn":
         this.attackedFields = [
-          [
-            this.curCol - 1,
-            this.color === "white" ? this.curRow + 1 : this.curRow - 1,
-          ],
-          [
-            this.curCol + 1,
-            this.color === "white" ? this.curRow + 1 : this.curRow - 1,
-          ],
+          [this.col - 1, this.color === "white" ? this.row + 1 : this.row - 1],
+          [this.col + 1, this.color === "white" ? this.row + 1 : this.row - 1],
         ].filter((el) => el[0] >= 1 && el[0] <= 8);
         break;
       default:
@@ -40,14 +33,15 @@ class Piece {
   render() {
     return `<img src=${this.color}-${this.name}.svg  />`;
   }
-  setName(name: string) {
+
+  setName(name: string, pieceState: PieceStateArr) {
     this.name = name;
-    this.setAttackFields(this.name);
+    this.legalMoves(pieceState);
   }
 
   changeCords(cords: [number, number]) {
-    this.curCol = cords[0];
-    this.curRow = cords[1];
+    this.col = cords[0];
+    this.row = cords[1];
 
     if (this.name.includes("pawn") || this.name.includes("king"))
       this.moved = true;
@@ -60,15 +54,15 @@ class Piece {
 
     switch (this.name) {
       case "pawn":
-        if (this.curRow === 1 || this.curRow === 8) break;
+        if (this.row === 1 || this.row === 8) break;
 
         this.attackedFields &&
           this.attackedFields.forEach((cords) => {
             if (
               pieceState.find(
                 (piece) =>
-                  piece.curCol === cords[0] &&
-                  piece.curRow === cords[1] &&
+                  piece.col === cords[0] &&
+                  piece.row === cords[1] &&
                   piece.color !== this.color
               )
             )
@@ -77,55 +71,53 @@ class Piece {
 
         const isBlocked = pieceState.find((obj) => {
           if (this.color === "white")
-            return obj.curCol === this.curCol && obj.curRow === this.curRow + 1;
+            return obj.col === this.col && obj.row === this.row + 1;
           if (this.color === "black")
-            return obj.curCol === this.curCol && obj.curRow === this.curRow - 1;
+            return obj.col === this.col && obj.row === this.row - 1;
         });
 
         if (isBlocked) break;
 
         legalMoves.push([
-          this.curCol,
-          this.color === "white" ? this.curRow + 1 : this.curRow - 1,
+          this.col,
+          this.color === "white" ? this.row + 1 : this.row - 1,
         ]);
 
         const isDoubleFieldMoveAvailable = () => {
           if (this.color === "white")
             return pieceState.find(
-              (obj) =>
-                obj.curCol === this.curCol && obj.curRow === this.curRow + 2
+              (obj) => obj.col === this.col && obj.row === this.row + 2
             );
 
           if (this.color === "black")
             return pieceState.find(
-              (obj) =>
-                obj.curCol === this.curCol && obj.curRow === this.curRow - 2
+              (obj) => obj.col === this.col && obj.row === this.row - 2
             );
         };
 
         if (!this.moved && !isDoubleFieldMoveAvailable())
           legalMoves.push([
-            this.curCol,
-            this.color === "white" ? this.curRow + 2 : this.curRow - 2,
+            this.col,
+            this.color === "white" ? this.row + 2 : this.row - 2,
           ]);
         break;
-      case "king":
-        legalMoves.push(...this.kingMoves(pieceState));
+      case "knight":
+        legalMoves.push(...this.knightMoves(pieceState));
+        break;
+      case "bishop":
+        legalMoves.push(...this.crossMoves(pieceState));
+        break;
+      case "rook":
+        legalMoves.push(...this.verticalMoves(pieceState));
+        legalMoves.push(...this.horizontalMoves(pieceState));
         break;
       case "queen":
         legalMoves.push(...this.crossMoves(pieceState));
         legalMoves.push(...this.verticalMoves(pieceState));
         legalMoves.push(...this.horizontalMoves(pieceState));
         break;
-      case "bishop":
-        legalMoves.push(...this.crossMoves(pieceState));
-        break;
-      case "knight":
-        legalMoves.push(...this.knightMoves(pieceState));
-        break;
-      case "rook":
-        legalMoves.push(...this.verticalMoves(pieceState));
-        legalMoves.push(...this.horizontalMoves(pieceState));
+      case "king":
+        legalMoves.push(...this.kingMoves(pieceState));
         break;
     }
     this.possibleMoves = legalMoves;
@@ -135,36 +127,36 @@ class Piece {
     const result: [number, number, string?][] = [];
     let isBlocking = false;
 
-    for (let curRow = this.curRow + 1; curRow <= 8; curRow++) {
+    for (let row = this.row + 1; row <= 8; row++) {
       const isOccupied = pieceState.find(
-        (piece) => piece.curCol === this.curCol && piece.curRow === curRow
+        (piece) => piece.col === this.col && piece.row === row
       );
 
       if (isOccupied && !isBlocking) {
         if (isOccupied.color === this.color) isBlocking = true;
         else {
           isBlocking = true;
-          result.push([this.curCol, curRow, "attack"]);
+          result.push([this.col, row, "attack"]);
         }
       }
 
-      !isBlocking && result.push([this.curCol, curRow]);
+      !isBlocking && result.push([this.col, row]);
     }
 
-    for (let curRow = this.curRow - 1; curRow >= 1; curRow--) {
+    for (let row = this.row - 1; row >= 1; row--) {
       const isOccupied = pieceState.find(
-        (piece) => piece.curCol === this.curCol && piece.curRow === curRow
+        (piece) => piece.col === this.col && piece.row === row
       );
 
       if (isOccupied) {
         if (isOccupied.color === this.color) return result;
         else {
-          result.push([this.curCol, curRow, "attack"]);
+          result.push([this.col, row, "attack"]);
           return result;
         }
       }
 
-      result.push([this.curCol, curRow]);
+      result.push([this.col, row]);
     }
     return result;
   }
@@ -173,36 +165,36 @@ class Piece {
     const result: [number, number, string?][] = [];
     let isBlocking = false;
 
-    for (let curCol = this.curCol + 1; curCol <= 8; curCol++) {
+    for (let col = this.col + 1; col <= 8; col++) {
       const isOccupied = pieceState.find(
-        (piece) => piece.curCol === curCol && piece.curRow === this.curRow
+        (piece) => piece.col === col && piece.row === this.row
       );
 
       if (isOccupied && !isBlocking) {
         if (isOccupied.color === this.color) isBlocking = true;
         else {
           isBlocking = true;
-          result.push([curCol, this.curRow, "attack"]);
+          result.push([col, this.row, "attack"]);
         }
       }
 
-      !isBlocking && result.push([curCol, this.curRow]);
+      !isBlocking && result.push([col, this.row]);
     }
 
-    for (let curCol = this.curCol - 1; curCol >= 1; curCol--) {
+    for (let col = this.col - 1; col >= 1; col--) {
       const isOccupied = pieceState.find(
-        (piece) => piece.curCol === curCol && piece.curRow === this.curRow
+        (piece) => piece.col === col && piece.row === this.row
       );
 
       if (isOccupied) {
         if (isOccupied.color === this.color) return result;
         else {
-          result.push([curCol, this.curRow, "attack"]);
+          result.push([col, this.row, "attack"]);
           return result;
         }
       }
 
-      result.push([curCol, this.curRow]);
+      result.push([col, this.row]);
     }
     return result;
   }
@@ -213,68 +205,64 @@ class Piece {
     const isBlockingLeftSide = [false, false];
     const isBlockingRightSide = [false, false];
 
-    for (let curCol = this.curCol - 1; curCol >= 1; curCol--) {
+    for (let col = this.col - 1; col >= 1; col--) {
       numOfIterations[0] += 1;
 
       const isOccupiedUp = pieceState.find(
         (piece) =>
-          piece.curCol === curCol &&
-          piece.curRow === this.curRow + numOfIterations[0]
+          piece.col === col && piece.row === this.row + numOfIterations[0]
       );
 
       const isOccupiedDown = pieceState.find(
         (piece) =>
-          piece.curCol === curCol &&
-          piece.curRow === this.curRow - numOfIterations[0]
+          piece.col === col && piece.row === this.row - numOfIterations[0]
       );
 
       if (isOccupiedUp && !isBlockingLeftSide[0]) {
         isBlockingLeftSide[0] = true;
         if (isOccupiedUp.color !== this.color)
-          result.push([curCol, this.curRow + numOfIterations[0], "attack"]);
+          result.push([col, this.row + numOfIterations[0], "attack"]);
       }
       if (isOccupiedDown && !isBlockingLeftSide[1]) {
         isBlockingLeftSide[1] = true;
         if (isOccupiedDown.color !== this.color)
-          result.push([curCol, this.curRow + numOfIterations[0], "attack"]);
+          result.push([col, this.row - numOfIterations[0], "attack"]);
       }
 
       !isBlockingLeftSide[0] &&
-        result.push([curCol, this.curRow + numOfIterations[0]]);
+        result.push([col, this.row + numOfIterations[0]]);
       !isBlockingLeftSide[1] &&
-        result.push([curCol, this.curRow - numOfIterations[0]]);
+        result.push([col, this.row - numOfIterations[0]]);
     }
 
-    for (let curCol = this.curCol + 1; curCol <= 8; curCol++) {
+    for (let col = this.col + 1; col <= 8; col++) {
       numOfIterations[1] += 1;
 
       const isOccupiedUp = pieceState.find(
         (piece) =>
-          piece.curCol === curCol &&
-          piece.curRow === this.curRow + numOfIterations[1]
+          piece.col === col && piece.row === this.row + numOfIterations[1]
       );
 
       const isOccupiedDown = pieceState.find(
         (piece) =>
-          piece.curCol === curCol &&
-          piece.curRow === this.curRow - numOfIterations[1]
+          piece.col === col && piece.row === this.row - numOfIterations[1]
       );
 
       if (isOccupiedUp && !isBlockingRightSide[0]) {
         isBlockingRightSide[0] = true;
         if (isOccupiedUp.color !== this.color)
-          result.push([curCol, this.curRow + numOfIterations[1], "attack"]);
+          result.push([col, this.row + numOfIterations[1], "attack"]);
       }
       if (isOccupiedDown && !isBlockingRightSide[1]) {
         isBlockingRightSide[1] = true;
         if (isOccupiedDown.color !== this.color)
-          result.push([curCol, this.curRow + numOfIterations[1], "attack"]);
+          result.push([col, this.row - numOfIterations[1], "attack"]);
       }
 
       !isBlockingRightSide[0] &&
-        result.push([curCol, this.curRow + numOfIterations[1]]);
+        result.push([col, this.row + numOfIterations[1]]);
       !isBlockingRightSide[1] &&
-        result.push([curCol, this.curRow - numOfIterations[1]]);
+        result.push([col, this.row - numOfIterations[1]]);
     }
 
     return result.filter((cords) => cords[1] >= 1 && cords[1] <= 8);
@@ -282,30 +270,18 @@ class Piece {
 
   knightMoves(pieceState: PieceStateArr) {
     const result: [number, number, string?][] = [];
-    result.push(
-      [this.curCol + 2, this.curRow + 1],
-      [this.curCol + 2, this.curRow - 1]
-    );
-    result.push(
-      [this.curCol - 2, this.curRow + 1],
-      [this.curCol - 2, this.curRow - 1]
-    );
+    result.push([this.col + 2, this.row + 1], [this.col + 2, this.row - 1]);
+    result.push([this.col - 2, this.row + 1], [this.col - 2, this.row - 1]);
 
-    result.push(
-      [this.curCol + 1, this.curRow + 2],
-      [this.curCol - 1, this.curRow + 2]
-    );
+    result.push([this.col + 1, this.row + 2], [this.col - 1, this.row + 2]);
 
-    result.push(
-      [this.curCol + 1, this.curRow - 2],
-      [this.curCol - 1, this.curRow - 2]
-    );
+    result.push([this.col + 1, this.row - 2], [this.col - 1, this.row - 2]);
 
     const filteredResult = result.filter((cords) => {
       const [col, row] = cords;
 
       const occupiedField = pieceState.find(
-        (piece) => piece.curCol === col && piece.curRow === row
+        (piece) => piece.col === col && piece.row === row
       );
 
       if (
@@ -324,7 +300,7 @@ class Piece {
         const [col, row] = cords;
 
         const occupiedField = pieceState.find(
-          (piece) => piece.curCol === col && piece.curRow === row
+          (piece) => piece.col === col && piece.row === row
         );
 
         if (occupiedField && occupiedField.color !== this.color)
@@ -338,25 +314,25 @@ class Piece {
   kingMoves(pieceState: PieceStateArr) {
     const result: [number, number, string?][] = [];
     result.push(
-      [this.curCol + 1, this.curRow + 1],
-      [this.curCol, this.curRow + 1],
-      [this.curCol - 1, this.curRow + 1]
+      [this.col + 1, this.row + 1],
+      [this.col, this.row + 1],
+      [this.col - 1, this.row + 1]
     );
     result.push(
-      [this.curCol + 1, this.curRow - 1],
-      [this.curCol, this.curRow - 1],
-      [this.curCol - 1, this.curRow - 1]
+      [this.col + 1, this.row - 1],
+      [this.col, this.row - 1],
+      [this.col - 1, this.row - 1]
     );
 
-    result.push([this.curCol - 1, this.curRow]);
+    result.push([this.col - 1, this.row]);
 
-    result.push([this.curCol + 1, this.curRow]);
+    result.push([this.col + 1, this.row]);
 
     const filteredResult = result.filter((cords) => {
       const [col, row] = cords;
 
       const occupiedField = pieceState.find(
-        (piece) => piece.curCol === col && piece.curRow === row
+        (piece) => piece.col === col && piece.row === row
       );
 
       if (
@@ -375,7 +351,7 @@ class Piece {
         const [col, row] = cords;
 
         const occupiedField = pieceState.find(
-          (piece) => piece.curCol === col && piece.curRow === row
+          (piece) => piece.col === col && piece.row === row
         );
 
         if (occupiedField && occupiedField.color !== this.color)
